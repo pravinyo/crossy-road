ce8aadc2260f1a8995f215acbecdd85b88025333
Pravin Tripathi <pravin.tripathi@ves.ac.in>
2018-08-07 14:01:24 +0530

    coin script and animation added

---
+++ Assets/Crossy Road/Scripts/Coin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+	public int coinValue = 1;
+	// Use this for initialization
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == "Player"){
+			Debug.Log("Player picked up the coin");
+			//update the coin count
+			Destroy(this.gameObject);
+		}
+	}
+}
